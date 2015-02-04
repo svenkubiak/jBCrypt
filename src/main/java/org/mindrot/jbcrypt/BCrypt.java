e01238373df3bf2dc9e0f773f8e643332fae746e
skubiak <sven.kubiak@maredit.de>
2015-02-04 09:47:56 +0100

    removed html tags

--- src/main/java/org/mindrot/jbcrypt/BCrypt.java
+++ src/main/java/org/mindrot/jbcrypt/BCrypt.java
@@ -21,38 +21,38 @@ import java.security.SecureRandom;
  * BCrypt implements OpenBSD-style Blowfish password hashing using
  * the scheme described in "A Future-Adaptable Password Scheme" by
  * Niels Provos and David Mazieres.
- * <p>
+ * 
  * This password hashing system tries to thwart off-line password
  * cracking using a computationally-intensive hashing algorithm,
  * based on Bruce Schneier's Blowfish cipher. The work factor of
  * the algorithm is parameterised, so it can be increased as
  * computers get faster.
- * <p>
+ * 
  * Usage is really simple. To hash a password for the first time,
  * call the hashpw method with a random salt, like this:
- * <p>
+ * 
  * <code>
- * String pw_hash = BCrypt.hashpw(plain_password, BCrypt.gensalt()); <br />
+ * String pw_hash = BCrypt.hashpw(plain_password, BCrypt.gensalt()); 
  * </code>
- * <p>
+ * 
  * To check whether a plaintext password matches one that has been
  * hashed previously, use the checkpw method:
- * <p>
+ * 
  * <code>
- * if (BCrypt.checkpw(candidate_password, stored_hash))<br />
- * &nbsp;&nbsp;&nbsp;&nbsp;System.out.println("It matches");<br />
- * else<br />
- * &nbsp;&nbsp;&nbsp;&nbsp;System.out.println("It does not match");<br />
+ * if (BCrypt.checkpw(candidate_password, stored_hash))
+ * &nbsp;&nbsp;&nbsp;&nbsp;System.out.println("It matches");
+ * else
+ * &nbsp;&nbsp;&nbsp;&nbsp;System.out.println("It does not match");
  * </code>
- * <p>
+ * 
  * The gensalt() method takes an optional parameter (log_rounds)
  * that determines the computational complexity of the hashing:
- * <p>
+ * 
  * <code>
- * String strong_salt = BCrypt.gensalt(10)<br />
- * String stronger_salt = BCrypt.gensalt(12)<br />
+ * String strong_salt = BCrypt.gensalt(10)
+ * String stronger_salt = BCrypt.gensalt(12)
  * </code>
- * <p>
+ * 
  * The amount of work increases exponentially (2**log_rounds), so 
  * each increment is twice as much work. The default log_rounds is
  * 10, and the valid range is 4 to 30.
